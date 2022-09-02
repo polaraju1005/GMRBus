b3255a83100dd0c2882c4a00a62a944d3aa6e28e
polaraju1005 <yalagantisaipolaraju@gmail.com>
2022-09-02 23:13:20 +0530

    Initial Commit

--- app/src/main/java/com/example/gmrbus/AdminRegisterActivity.kt
+++ app/src/main/java/com/example/gmrbus/AdminRegisterActivity.kt
@@ -11,17 +11,17 @@ import java.time.temporal.TemporalAdjusters.next
 
 class AdminRegisterActivity : AppCompatActivity() {
 
-    lateinit var logo: ImageView
-    lateinit var txtBuses: TextView
-    lateinit var textHey: TextView
+    lateinit var adLogo: ImageView
+    lateinit var adTxtBuses: TextView
+    lateinit var adTextHey: TextView
     private lateinit var imgAdmin: ImageView
-    private lateinit var edtUserName: EditText
-    private lateinit var edtEmail: EditText
-    lateinit var etPersonalPhone: EditText
+    private lateinit var edtAdminName: EditText
+    private lateinit var edtAdminEmail: EditText
+    lateinit var etAdminPhone: EditText
     private lateinit var busroute: TextInputLayout
-    private lateinit var etpassword: EditText
-    lateinit var etConfirmPassword: EditText
-    lateinit var register: Button
+    private lateinit var etAdPassword: EditText
+    lateinit var etAdConfirmPassword: EditText
+    lateinit var adRegister: Button
     private lateinit var name: String
     lateinit var mail: String
     lateinit var password: String
@@ -37,32 +37,32 @@ class AdminRegisterActivity : AppCompatActivity() {
 
         title = "Register"
 
-        logo = findViewById(R.id.imgGMR)
-        txtBuses = findViewById(R.id.txtBuses)
-        textHey = findViewById(R.id.txtHey)
+        adLogo = findViewById(R.id.adImgGMR)
+        adTxtBuses = findViewById(R.id.adTxtBuses)
+        adTextHey = findViewById(R.id.adTxtHey)
         imgAdmin = findViewById(R.id.imgAdmin)
-        edtUserName = findViewById(R.id.etUsername)
-        edtEmail = findViewById(R.id.etEmail)
+        edtAdminName = findViewById(R.id.etAdminName)
+        edtAdminEmail = findViewById(R.id.etAdminEmail)
         busroute = findViewById(R.id.dropdownBusRoute)
-        etPersonalPhone = findViewById(R.id.etPersonalPhn)
-        etpassword = findViewById(R.id.etPassword)
-        etConfirmPassword = findViewById(R.id.etCnfPassword)
-        register = findViewById(R.id.btnRegister)
+        etAdminPhone = findViewById(R.id.etAdminPhn)
+        etAdPassword = findViewById(R.id.etAdPassword)
+        etAdConfirmPassword = findViewById(R.id.etAdCnfPassword)
+        adRegister = findViewById(R.id.btnAdRegister)
 
-        val busroutes = resources.getStringArray(R.array.busroute)
+        val busRoutes = resources.getStringArray(R.array.busroute)
 
-        val arrayAdapter = ArrayAdapter(this, R.layout.dropdown_busroute, busroutes)
+        val arrayAdapter3 = ArrayAdapter(this, R.layout.dropdown_busroute, busRoutes)
 
-        val autocompleteTV = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView)
+        val autocompleteTV3 = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView2)
 
-        autocompleteTV.setAdapter(arrayAdapter)
+        autocompleteTV3.setAdapter(arrayAdapter3)
 
-        register.setOnClickListener {
-            name = edtUserName.text.toString().trim { it <= ' ' }
-            mail = edtEmail.text.toString().trim { it <= ' ' }
-            personalPhone = etPersonalPhone.text.toString().trim { it <= ' ' }
-            password = etpassword.text.toString().trim { it <= ' ' }
-            confirmPassword = etConfirmPassword.text.toString().trim { it <= ' ' }
+        adRegister.setOnClickListener {
+            name = edtAdminName.text.toString().trim { it <= ' ' }
+            mail = edtAdminEmail.text.toString().trim { it <= ' ' }
+            personalPhone = etAdminPhone.text.toString().trim { it <= ' ' }
+            password = etAdPassword.text.toString().trim { it <= ' ' }
+            confirmPassword = etAdConfirmPassword.text.toString().trim { it <= ' ' }
             if (name.isEmpty()) {
                 Toast.makeText(this, "Please enter name", Toast.LENGTH_SHORT).show()
             } else if (mail.isEmpty()) {
