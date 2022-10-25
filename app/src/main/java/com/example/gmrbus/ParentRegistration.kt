07ec35787e9eb5d5f27a13bba91bd5975c3f51c4
polaraju1005 <yalagantisaipolaraju@gmail.com>
2022-10-25 15:02:06 +0530

    initial commit

---
+++ app/src/main/java/com/example/gmrbus/ParentRegistration.kt
@@ -0,0 +1,11 @@
+package com.example.gmrbus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ParentRegistration : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_parent_registration)
+    }
+}
